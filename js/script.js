2e0909c8b9fd6a1890ebc99fef5d5250d3610b31
spigkaht <benj@betterrentals.com.au>
2024-02-12 21:58:26 +1100

    Completed Etch-a-Sketch exercise for Odin JavaScript Exercises
    Reset sketchpad interactive button added
    with user input for size of grid

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 const gridContainer = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-  const gridRow = document.createElement("div");
-  gridRow.className = "row";
-  gridContainer.appendChild(gridRow);
-  
-  for (let j = 0; j < 15; j++) {
-    const gridColumn = document.createElement("div");
-    gridColumn.className = "column";
-    gridRow.appendChild(gridColumn);
-  }
+function createMouseOver() {
+  [...document.querySelectorAll(".column")].forEach(function (item) {
+    item.addEventListener("mouseover", function () {
+      item.style.backgroundColor = "black";
+    });
+  });
 }
 
-[...document.querySelectorAll(".column")].forEach(function(item) {
-  item.addEventListener("mouseover", function() {
-    item.style.backgroundColor = "red";
-  });
-});
+function setGrid(gridSize) {
+  console.log("Setting Grid");
+  console.log(gridContainer);
+  for (let i = 0; i < gridSize; i++) {
+    const gridRow = document.createElement("div");
+    gridRow.className = "row";
+    gridContainer.appendChild(gridRow);
+
+    for (let j = 0; j < gridSize; j++) {
+      const gridColumn = document.createElement("div");
+      gridColumn.className = "column";
+      gridRow.appendChild(gridColumn);
+    }
+  }
+  createMouseOver();
+}
+
+setGrid(16);
+
+function resetScreen() {
+  document.querySelector("#container").innerHTML = "";
+  const gridSize = prompt("Enter a grid size (1-100):");
+
+  if (gridSize > 100 || gridSize < 1) {
+    alert("Incorrect entry. Enter 1-100 only");
+    resetScreen();
+  }
+
+  setGrid(gridSize);
+}
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetScreen);
